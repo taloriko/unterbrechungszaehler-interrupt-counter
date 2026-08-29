0e8875e4edbc8d61cbd663eee6983c7ac73641e3
taloriko <67206692+taloriko@users.noreply.github.com>
2026-08-29 14:28:00 +0200

    release: set runtime version 2026-08-29-17

--- arduino/UnterbrechungszaehlerInterruptCounter/Version.ino
+++ arduino/UnterbrechungszaehlerInterruptCounter/Version.ino
@@ -1,7 +1,8 @@
 // Version.ino
 // Runtime version source for all UI/API/serial output.
+// TODO: remove after APP_VERSION is consolidated directly in the main sketch.
 
-static const char* FIRMWARE_VERSION = "2026-08-29-16";
+static const char* FIRMWARE_VERSION = "2026-08-29-17";
 
 class FirmwareVersionInitializer {
 public:
