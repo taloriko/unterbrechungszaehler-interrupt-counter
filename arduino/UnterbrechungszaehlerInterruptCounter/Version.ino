ea3779dfb11c6b31abe63bb00888ebe1a6d6a22d
taloriko <67206692+taloriko@users.noreply.github.com>
2026-08-29 17:29:32 +0200

    Bump firmware version for RTC and OLED support

--- arduino/UnterbrechungszaehlerInterruptCounter/Version.ino
+++ arduino/UnterbrechungszaehlerInterruptCounter/Version.ino
@@ -2,7 +2,7 @@
 // Runtime version source for all UI/API/serial output.
 // TODO: remove after APP_VERSION is consolidated directly in the main sketch.
 
-static const char* FIRMWARE_VERSION = "2026-08-29-17";
+static const char* FIRMWARE_VERSION = "2026-08-29-18";
 
 class FirmwareVersionInitializer {
 public:
