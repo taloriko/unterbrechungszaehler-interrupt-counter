4efa5538ae304ef6a5bcaaa45af60a07b806c395
taloriko <67206692+taloriko@users.noreply.github.com>
2026-08-29 18:10:56 +0200

    Bump firmware version to 2026-08-29-20

--- arduino/UnterbrechungszaehlerInterruptCounter/Version.ino
+++ arduino/UnterbrechungszaehlerInterruptCounter/Version.ino
@@ -2,7 +2,7 @@
 // Runtime version source for all UI/API/serial output.
 // TODO: remove after APP_VERSION is consolidated directly in the main sketch.
 
-static const char* FIRMWARE_VERSION = "2026-08-29-19";
+static const char* FIRMWARE_VERSION = "2026-08-29-20";
 
 class FirmwareVersionInitializer {
 public:
