b4ade3b7d110569c690f9197ec3ca26246ad376b
taloriko <67206692+taloriko@users.noreply.github.com>
2026-08-29 20:20:38 +0200

    Make heatmap aggregates incremental

--- arduino/UnterbrechungszaehlerModular/AnalyticsService.h
+++ arduino/UnterbrechungszaehlerModular/AnalyticsService.h
@@ -11,6 +11,7 @@ public:
   void begin(StorageService* storage);
   bool ensureBaseAggregates();
   bool ensureSelectedWeek(int isoYear, int isoWeek);
+  bool warmCurrent();
 
   int baseYear() const { return baseYear_; }
   uint16_t weekdayHour(uint8_t day, uint8_t hour) const;
@@ -26,9 +27,12 @@ private:
   void clearBase();
   int determineBaseYear();
   void increment(uint16_t& value);
+  bool addEpochToBase(uint32_t epoch);
+  bool addEpochToSelectedWeek(uint32_t epoch, int selectedYear, int selectedWeek);
 
   StorageService* storage_ = nullptr;
   uint32_t cachedRevision_ = 0xFFFFFFFFUL;
+  uint32_t cachedArchiveCount_ = 0;
   int baseYear_ = 2026;
   uint16_t weekdayHour_[7][24] = {};
   uint16_t monthWeek_[UicConfig::LONGTERM_CACHE_YEARS][12][53] = {};
@@ -36,6 +40,7 @@ private:
   bool yearUsed_[UicConfig::LONGTERM_CACHE_YEARS] = {};
 
   uint32_t selectedRevision_ = 0xFFFFFFFFUL;
+  uint32_t selectedArchiveCount_ = 0;
   int selectedYear_ = 0;
   int selectedWeek_ = 0;
   uint16_t selectedWeekdayHour_[7][24] = {};
