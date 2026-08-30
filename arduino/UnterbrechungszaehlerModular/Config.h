f01ddd3e9a2fb019ac4a7110e3b3cbd950ba2667
taloriko <67206692+taloriko@users.noreply.github.com>
2026-08-30 12:05:13 +0200

    Bump modular reboot version to 16

--- arduino/UnterbrechungszaehlerModular/Config.h
+++ arduino/UnterbrechungszaehlerModular/Config.h
@@ -4,7 +4,7 @@
 
 namespace UicConfig {
 
-static constexpr char APP_VERSION[] = "2026-08-30-reboot-15";
+static constexpr char APP_VERSION[] = "2026-08-30-reboot-16";
 static constexpr char HOSTNAME[] = "unterbrechungen";
 static constexpr char FALLBACK_AP_SSID[] = "Unterbrechungszaehler";
 static constexpr char TZ_INFO[] = "CET-1CEST,M3.5.0,M10.5.0/3";
