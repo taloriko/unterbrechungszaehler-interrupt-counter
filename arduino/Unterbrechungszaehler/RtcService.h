f49250e9b1dcda76df3f71af0bf505df163d1fe4
taloriko <67206692+taloriko@users.noreply.github.com>
2026-09-01 20:41:45 +0200

    2.1.2: RTC Cache und Diagnosewerte

--- arduino/Unterbrechungszaehler/RtcService.h
+++ arduino/Unterbrechungszaehler/RtcService.h
@@ -6,25 +6,40 @@
 class RtcService {
 public:
   void begin();
+  void tick();
+
   bool present() const { return present_; }
   bool timeValid() const { return timeValid_; }
   bool oscillatorStopFlag() const { return oscillatorStopFlag_; }
+  bool communicationOk() const { return communicationOk_; }
+  float temperatureC() const { return temperatureC_; }
+  uint32_t lastCheckAt() const { return lastCheckAt_; }
+  uint32_t lastCheckAgeMs() const;
+  int32_t systemDifferenceSeconds() const { return systemDifferenceSeconds_; }
 
   bool applyToSystem();
   bool writeSystemTime();
   bool read(struct tm& value, bool& oscillatorStopFlag);
 
-  String dateText();
-  String timeText();
+  String dateText() const;
+  String timeText() const;
 
 private:
   uint8_t bcdToDec(uint8_t value) const;
   uint8_t decToBcd(uint8_t value) const;
   bool readRegister(uint8_t reg, uint8_t& value);
   bool writeRegister(uint8_t reg, uint8_t value);
-  void refreshState();
+  bool refreshState();
+  bool readTemperature(float& value);
+  void updateDifference();
 
   bool present_ = false;
   bool timeValid_ = false;
   bool oscillatorStopFlag_ = false;
+  bool communicationOk_ = false;
+  float temperatureC_ = NAN;
+  struct tm cachedTime_ = {};
+  bool cachedTimeAvailable_ = false;
+  uint32_t lastCheckAt_ = 0;
+  int32_t systemDifferenceSeconds_ = 0;
 };
