1001f0a8d6689225fc250b5c85bc8aef0683be11
taloriko <67206692+taloriko@users.noreply.github.com>
2026-09-01 22:04:41 +0200

    2.1.2: RTC-Cache und periodische Diagnose

--- arduino/Unterbrechungszaehler/RtcService.h
+++ arduino/Unterbrechungszaehler/RtcService.h
@@ -6,25 +6,37 @@
 class RtcService {
 public:
   void begin();
+  bool tick();
+  bool checkDue() const;
+
   bool present() const { return present_; }
   bool timeValid() const { return timeValid_; }
   bool oscillatorStopFlag() const { return oscillatorStopFlag_; }
+  bool communicationOk() const { return communicationOk_; }
+  uint32_t lastCheckAgeMs() const;
+  float temperatureC() const { return temperatureC_; }
 
   bool applyToSystem();
   bool writeSystemTime();
   bool read(struct tm& value, bool& oscillatorStopFlag);
 
-  String dateText();
-  String timeText();
+  String dateText() const;
+  String timeText() const;
 
 private:
   uint8_t bcdToDec(uint8_t value) const;
   uint8_t decToBcd(uint8_t value) const;
   bool readRegister(uint8_t reg, uint8_t& value);
   bool writeRegister(uint8_t reg, uint8_t value);
-  void refreshState();
+  bool sample();
+  bool cachedLocalTime(struct tm& value) const;
 
   bool present_ = false;
   bool timeValid_ = false;
   bool oscillatorStopFlag_ = false;
+  bool communicationOk_ = false;
+  float temperatureC_ = NAN;
+  time_t cachedEpoch_ = 0;
+  uint32_t cachedAtMs_ = 0;
+  uint32_t lastCheckAtMs_ = 0;
 };
