e0755667af43a111eb13b20612d889db75ca3c25
taloriko <67206692+taloriko@users.noreply.github.com>
2026-09-15 15:27:15 +0200

    Expose work-cycle button diagnostics

--- Unterbrechungszaehler/work_cycle.h
+++ Unterbrechungszaehler/work_cycle.h
@@ -1,8 +1,14 @@
 #pragma once
 
+#include <Arduino.h>
+
 namespace WorkCycle {
 
 void begin();
 void update();
+
+uint32_t suppressedPhysicalPressCount();
+bool hasSuppressedPhysicalPress();
+uint32_t lastSuppressedPhysicalPressMs();
 
 }  // namespace WorkCycle
